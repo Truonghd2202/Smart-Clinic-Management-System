7b6396f52daa0ef19193349822a34126e03aeb4f
Truonghd2202 <dtruong2202@gmail.com>
2025-10-22 08:51:04 +0700

    Implement AppointmentService for booking appointments

---
+++ src/main/java/com/project/backend/services/AppointmentService.java
@@ -0,0 +1,19 @@
+package com.project.back_end.services;
+
+import com.project.back_end.models.Appointment;
+import com.project.back_end.repositories.AppointmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+
+    private final AppointmentRepository appointmentRepository;
+
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public Appointment bookAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+}
